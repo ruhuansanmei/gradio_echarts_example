98f1cbd4c5cf7ccd2a09f238afe6a6a79426d415
dazongshi <2273056394@qq.com>
2024-07-14 10:30:53 +0800

    init: store project and share to all

---
+++ README.md
@@ -0,0 +1,24 @@
+# project(项目)
+一个最小化的echarts组件示例
+one small echrarts gradio component example
+
+# env(环境)
+Python 3.12.4
+node v20.15.1
+npm 10.7.0
+
+# gradio_mycomponent
+
+You can auto-generate documentation for your custom component with the `gradio cc docs` command.
+You can also edit this file however you like.
+
+# run dev（启动运行）
+
+`gradio cc dev`
+
+# the running pic(运行的样子)
+![alt text](image.png)
+
+# ref(参考)
+https://www.gradio.app/guides/custom-components-in-five-minutes
+https://echarts.apache.org/handbook/zh/basics/import/#
---
+++ demo/app.py
@@ -0,0 +1,36 @@
+
+import gradio as gr
+from gradio_mycomponent import MyComponent
+import json
+
+example = MyComponent().example_value()
+
+with gr.Blocks() as demo:
+    myComponent = MyComponent()
+
+def transform_data(data, ratio):
+    'transform data with ratio'
+    '转换原始数据'
+    obj =  json.loads(data)
+    for serie in obj.get('series'):
+        data = serie.get('data')
+        if data != None:
+            new_data = list(map(lambda x: x * ratio, data))
+            serie['data'] = new_data
+    return json.dumps(obj)
+
+
+demo = gr.Interface(
+    transform_data,
+    inputs=[gr.Text(value=r'{"title":{"text":"ECharts示例,可按倍率调整数据(draw with ratio changed data)"},"tooltip":{},"xAxis":{"data":["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]},"yAxis":{},"series":[{"name":"销量","type":"bar","data":[5,20,36,10,10,20]}]}', label='原始数据(raw data)'),
+             gr.Slider(value=2, minimum=1, maximum=10, step=1, label= "倍数(ratio)")],
+    #   outputs =["text"],   # interactive version of your component
+    outputs= MyComponent(),  # static version of your component
+    # examples=[[example]],  # uncomment this line to view the "example version" of your component
+    submit_btn= "绘图(draw)",
+    clear_btn= "清除数据(clear)"
+)
+
+
+if __name__ == "__main__":
+    demo.launch()
